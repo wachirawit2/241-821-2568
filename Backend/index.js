bf2a97f4922350687f81e630b55e7b34cbe67fa8
wachirawit2 <wachirawit.ou@ku.th>
2026-02-24 12:37:53 +0700

    api express update

--- Backend/index.js
+++ Backend/index.js
@@ -1,88 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
 const app = express();
 const port = 8000;
-
 app.use(bodyParser.json());
-
 let users = []
 let counter = 1;
 
-//path = GET /users
-app.get('/users',  (req, res) => {
-    res.json(users);
-});
-
-//path = POST /user
-app.post('/user', (req, res) => {
-    let user = req.body;
-    user.id = counter
-    counter += 1
-    users.push(user);
-    res.json({ 
-        message: 'User added successfully', 
-        user: user });
-})
-
-//path = PUT /user/:id
-app.patch('/user/:id', (req, res) => {
-    let id = req.params.id
-    let updatedUser = req.body;
-    // หา users จาก id
-    let selectedIndex = users.findIndex(user => user.id == id)
-    //update users นั้น
-    if (updatedUser.name) {
-        users[selectedIndex].name = updatedUser.name    
-    }
-    if (updatedUser.age) {
-        users[selectedIndex].age = updatedUser.age
-    }
-
-    //ส่ง response กลับไปว่า update users ที่เลือกสำเร็จแล้ว
-
-    res.json({
-        message: 'User updated successfully',
-        data : {
-            user: updatedUser,
-            indexUpdated: selectedIndex
-        }
-    })  
-})
-
-//path = DELETE /users/:id
-app.delete('/users/:id', (req, res) => {
-    let id = req.params.id
-    let selectedIndex = users.findIndex(user => user.id == id)
-    if (selectedIndex !== -1) {
-        users.splice(selectedIndex, 1)
-        res.json({
-            message: 'User deleted successfully',
-            data: {
-                indexDeleted: selectedIndex
-            }
-        })
-    } else {
-        res.status(404).json({
-            message: 'User not found'
-        })
-    }
-})
-
-//path = delete /users
-app.delete('/users', (req, res) => {
-    // หา index ของ users ทีต้องการลบ id ที่ส่งมา
-    let seletedIndex = users.findIndex(user => user.id == id);
-    // ลบ user จาก array โดยใช้ delete
-    user.splice(seletedIndex,1);
-    // ส่ง response กลับไปว่า delete user ที่เลือกสำเร็จแล้ว
-    res.json({
-        message: 'User deleted successfully',
-        data: {
-            indexDeleted: seletedIndex
-        }
+let conn = null
+const initDbConnection = async () => {
+    conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8821
     })
+}
+
+// path = GET users สำหรับ get ข้อมูล users ทั้งหมด
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(port, async () => {
+    await initDbConnection();
+    console.log(`Server is running at http://localhost:${port}`);
 });
